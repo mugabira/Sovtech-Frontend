4cdb5221da441c95196ba61759773ba1472ade83
mugabira <mullegow@gmail.com>
2021-05-16 14:13:42 +0300

    Heroku compartibility

--- sovtech-frontend/src/environments/environment.dev.ts
+++
@@ -1,8 +0,0 @@
-export const environment = {
-    production: false,
-    categoriesAPIUrl: 'https://localhost:44394/api/Chuck/api/Chuck/categories',
-    jandomJokeAPIUrl: 'https://api.chucknorris.io/jokes/random?category=',
-    peopleAPIUrl: 'https://localhost:44394/api/Swapi/api/Swapi/people',
-    searchAPIUrl: 'https://localhost:44394/api/Search?searchTerm='
-  
-  };
--- sovtech-frontend/src/environments/environment.prod.ts
+++
@@ -1,7 +0,0 @@
-export const environment = {
-  production: true,
-  categoriesAPIUrl: 'https://api.chucknorris.io/jokes/categories',
-  jandomJokeAPIUrl: 'https://api.chucknorris.io/jokes/random?category=',
-  peopleAPIUrl: 'https://swapi.dev/api/people/?format=json',
-  searchAPIUrl: 'https://localhost:44394/api/Search?searchTerm='
-};
